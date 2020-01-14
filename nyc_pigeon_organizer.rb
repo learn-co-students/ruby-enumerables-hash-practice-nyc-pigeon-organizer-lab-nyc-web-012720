045d2c78163bba918514ab3868c0d5243e9faf60
Adit P Patel <app2116@gmail.com>
2020-01-14 23:04:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.map{|k,v|
+    v.map{|internal_key, value| 
+    puts value
+      value.each_with_index{|name, i| 
+      result[name] = {} unless result[name]
+      result[name][k] = Array.new() unless result[name][k]
+      result[name][k] << internal_key.to_s
+     }
+    }
+  }
+    return result
 end
