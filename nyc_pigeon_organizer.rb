60ecdc0a82db136948a7dbe14e53d6490261215b
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-22 03:56:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data.each_pair do |att, inner_hash| 
+    inner_hash.each_pair do |att_val, name_arr|
+      name_arr.each do |name| 
+        att_val_arr = []
+        att_val_arr << att_val.to_s
+      
+        if new_hash[name] && new_hash[name][att]
+          new_hash[name][att] << att_val.to_s 
+          
+        elsif new_hash[name] && !new_hash[name][att]
+          new_hash[name][att] = att_val_arr
+          
+        else 
+          small_hash = {}
+          small_hash[att] = att_val_arr
+          new_hash[name] = small_hash
+        end 
+      end 
+    end 
+  end 
+  
+new_hash  
+    
 end
